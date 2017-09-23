fcc23d2244245acd02d8cca30b81d205022a8c61
Kuba W <31992970+jakwer84@users.noreply.github.com>
2017-09-23 11:06:08 +0200

    added comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
-console.log('Hello, ' + name); 
+//
+//Below I have commented on a function call to disable it
+// console.log('Hello, ' + name) ('console will not be called');
